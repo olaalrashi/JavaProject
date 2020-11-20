4ff8a3876e4c17337ae69d1f018821985f6dcded
ola <olaalrashi@gmail.com>
2020-11-20 13:57:42 -0800

    edit

--- JavaProject/src/javaproject/JavaProject.java
+++ JavaProject/src/javaproject/JavaProject.java
@@ -16,7 +16,8 @@ public class JavaProject {
      */
     public static void main(String[] args) {
         
-        System.out.println("abd");
+        System.out.println("ola");
+
         // TODO code application logic here
     }
    
